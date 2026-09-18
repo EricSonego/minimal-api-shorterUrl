0dd62f78e2a72fe09a7677b7d7a275361462e202
Eric <eeericsonego@gmail.com>
2026-09-17 23:23:20 -0300

    feat: migrations init

---
+++ Migrations/20260918022159_InitialCreate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace minimal_api_shorterUrl.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UrlMappings",
+                columns: table => new
+                {
+                    IdUrl = table.Column<Guid>(type: "TEXT", nullable: false),
+                    LongUrl = table.Column<string>(type: "TEXT", nullable: false),
+                    ShortCode = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UrlMappings", x => x.IdUrl);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UrlMappings_ShortCode",
+                table: "UrlMappings",
+                column: "ShortCode",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UrlMappings");
+        }
+    }
+}
