8f5a3dca0fc43f242b02f639092b2431955b12ac
Eric Sonego <eeericsonego@gmail.com>
2026-09-18 00:02:35 -0300

    feat: add service url

---
+++ Services/UrlShortenerService.cs
@@ -0,0 +1,24 @@
+﻿namespace minimal_api_shorterUrl.Services
+{
+    public class UrlShortenerService
+    {
+        // valid characters
+        private const string Alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        private readonly Random _random;
+
+        public string GenerateCode()
+        {
+            var codeChars = new char[6];
+            for (int i = 0; i < 6; i++)
+            {
+                // choose random position
+                int randomIndex = _random.Next(Alphabet.Length);
+                codeChars[i] = Alphabet[randomIndex];
+            }
+
+            // array to string
+            return new string(codeChars);
+        }
+
+    }
+}
